323e7403a67517ae6b8ba2464630fc2bf72fd21b
Jeff Slavin <Jeff.Slavin@outlook.com>
2020-03-03 23:15:07 -0700

    Setting up the prototype object lesson complete

--- OOP/person.js
+++ OOP/person.js
@@ -1,18 +1,46 @@
+// Prototypal Inheritance (different than c++ inheritance)
+
 // Constructor
-const Person = function(firstName, lastName, age) {
+// Likes has a default of an empty array
+const Person = function(firstName, lastName, age, likes = []) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
+    this.likes = likes;
 };
 
-//const me = Person();
-const me = new Person('Jenn', 'Slavin', 37);
+// Member method
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`;
 
-console.log(me);
-console.log(typeof me);
-console.log(me.age);
-me.firstName = 'Jeff';
-console.log(me);
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`;
+    });
+
+    return bio;
+};
+
+// Member method
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ');  // splits the fullName by a space (returns an array with 2 items)
+    this.firstName = names[0];
+    this.lastName = names[1];
+};
+
+// Members do not need to be methods. This creates a non function member with the value of 'Thailand' for all instances of Person
+// Usually does not serve much of a purpose
+// Person.prototype.location = 'Thailand';
+
+//const me = Person();
+const me = new Person('Jenn', 'Slavin', 37, ['Teaching', 'Biking']);
+me.setName('Noah Slavin');
+// console.log(me);
+// console.log(typeof me);
+// console.log(me.age);
+// me.firstName = 'Jeff';
+// console.log(me);
+console.log(me.getBio());
 
 const person2 = new Person('Jake', 'Davis', 40);
-console.log(person2);
+// console.log(person2);
+console.log(person2.getBio());
