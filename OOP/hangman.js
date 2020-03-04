01b217a261b363ef5ef25e12c774051701ccb27a
Jeff Slavin <Jeff.Slavin@outlook.com>
2020-03-03 23:26:45 -0700

    Hangman challenege part 1 lesson complete

--- OOP/hangman.js
+++ OOP/hangman.js
@@ -1,11 +1,29 @@
 // Constructor
 const Hangman = function (word, remainingGuesses) {
-    this.word = word;
+    this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
+    this.guessedLetters = ['c', 'e'];
+};
+
+// Member Method
+Hangman.prototype.getPuzzle = function () {
+
+    let puzzle = '';
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        } else {
+            puzzle += '*';
+        };
+    });
+
+    return puzzle;
+
 };
 
 const game1 = new Hangman('cat', 2);
 const game2 = new Hangman('New Jersey', 4);
 
-console.log(game1);
-console.log(game2);
+console.log(game1.getPuzzle());
+console.log(game2.getPuzzle());
