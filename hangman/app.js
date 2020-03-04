db31908997f20a20f3f0e1317374c1e98868f5e1
Jeff Slavin <Jeff.Slavin@outlook.com>
2020-03-04 13:31:23 -0700

    Primitives and objects part 2 lesson complete

--- hangman/app.js
+++ hangman/app.js
@@ -1,11 +1,28 @@
 // Inheritance tree
 // Look at product first
 // Then look at the prototype
-// product --> Object.prototype --> null
+// Prototype chain for an object: myObject --> Object.prototype --> null
+// Primitive Value: a value that does not have properties (non object)
+// There are 5 of these in JavaScript: string, number, boolean, null, undefined
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunction --> Function.prototype --> Object.prototype --> null
 
-const product = {
-    name: 'The War Of Art'
-};
+const product = 'Computer';
+console.log(product);
+
+const otherProduct = new String('Phone');
+console.log(otherProduct);
+
+// const getScore = () => 1;
+// console.log(getScore);
+
+// const team = ['Luke', 'Madison'];
+// const team = new Array(['Luke', 'Madison']);
+// console.log(team.hasOwnProperty('filter'));
+// console.log(team);
+// const product = {
+//     name: 'The War Of Art'
+// };
 
 // This is the same as the above (using the 'new' and object constructor)
 // Don't need to do this, because JS handles this correctly without 'new' on built in types
@@ -15,10 +32,10 @@ const product = {
 
 // Generally a horrible idea, but you can override the behavior of the object prototype
 //Object.prototype.hasOwnProperty = () => 'This is the new function';
-Object.prototype.someNewMethod = () => 'This is a new method';
+// Object.prototype.someNewMethod = () => 'This is a new method';
 // hasOwnProperty - returns true or false if the object has that properpty
 // hasOwnProperty - only looks at the object (in this case product) does not then look up the prototype chain
 // Since this function lives up the prototype chain, the below returns false
-console.log(product.hasOwnProperty('hasOwnProperty'));
-console.log(product.someNewMethod());
-console.log(product);
+// console.log(product.hasOwnProperty('hasOwnProperty'));
+// console.log(product.someNewMethod());
+// console.log(product);
