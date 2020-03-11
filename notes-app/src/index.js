8f5ba0d5e7589ab207f7e82eca12c45355bc5bb3
Jeff Slavin <Jeff.Slavin@outlook.com>
2020-03-10 23:16:02 -0600

    Converting notes app part 3 lesson complete

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,20 @@
-import { getNotes, createNote } from './notes';
+import { getNotes, createNote, removeNote, updateNote } from './notes';
+import { getFilters, setFilters } from './filters';
 
-console.log(getNotes());
-createNote();
-console.log(getNotes());
+// console.log(getNotes());
+// createNote();
+// removeNote('1');
+
+// updateNote('ab798bb7-0f24-484a-be19-8ac2cc5e18ac', {
+//     title: 'My note title',
+//     body: 'This is the body for my note'
+// });
+
+// console.log(getNotes());
+
+console.log(getFilters());
+setFilters({
+    searchText: 'Office',
+    sortBy: 'byCreated'
+});
+console.log(getFilters());
