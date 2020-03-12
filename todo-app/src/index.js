8b08a6fc8f24951ca9563782cf732aeb2a083fc5
Jeff Slavin <Jeff.Slavin@outlook.com>
2020-03-11 21:41:59 -0600

    Converting to-do app part 1 lesson complete

---
+++ todo-app/src/index.js
@@ -0,0 +1,7 @@
+import { getTODOs, createTODO, removeTODO, toggleTODO } from './todos';
+
+console.log(getTODOs());
+
+removeTODO('2ee53fca-0d27-4e58-9c3e-a8ddae96c6d8');
+
+console.log(getTODOs());
